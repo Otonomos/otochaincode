4d538c5f6d5e68cf72ae3aa93e957aef4a131646
Mano Thanabalan <mano.thanabalan@gmail.com>
2016-11-01 16:39:35 +0800

    Update vehicles.go

--- vehicles.go
+++ vehicles.go
@@ -70,8 +70,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface) ([]byte, error
 }
 
 func main() {
-	//err := shim.Start(new(SimpleChaincode))
-	//if err != nil {
-		//fmt.Printf("Error starting Simple chaincode: %s", err)
-	//}
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
 }
